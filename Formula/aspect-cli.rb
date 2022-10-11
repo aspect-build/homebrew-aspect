ba266d4b9bbf20ef678b651b84673d7e4834d447
Chuck Grindel <chuck@chuckgrindel.com>
2022-10-11 17:06:58 -0600

    Build from silo

--- Formula/aspect-cli.rb
+++ Formula/aspect-cli.rb
@@ -6,8 +6,12 @@ class AspectCli < Formula
   # sha256 "836a6a5d94e6ae50a1a274b04f0b17125a9b843b19df010a502ef9bf7ddb679f"
   # url "https://github.com/aspect-build/aspect-cli/archive/0413f763438d027be9afc816e3c1929d39993ac5.tar.gz"
   # sha256 "256c90adbdfd5703c28aefa6d0644b5c1a90171e7b231a0ff53583ec08549e24"
-  url "https://github.com/aspect-build/aspect-cli/archive/d611bbb696cedea7e1dcb5f02b957e8323469c8f.tar.gz"
-  sha256 "65d4ea42f91edd0a04a7da692479598345249ef415a8568e43f72e5ead4ebc67"
+  # url "https://github.com/aspect-build/aspect-cli/archive/d611bbb696cedea7e1dcb5f02b957e8323469c8f.tar.gz"
+  # sha256 "65d4ea42f91edd0a04a7da692479598345249ef415a8568e43f72e5ead4ebc67"
+
+  url "https://github.com/aspect-build/silo.git",
+    revision: "f4735da850fff0168001ac72fa1c0ab5e1fa8463"
+
   license "Apache-2.0"
 
   # TODO(chuck): REMOVE THIS!
@@ -16,7 +20,8 @@ class AspectCli < Formula
   depends_on "bazelisk" => :build
 
   def install
-    system "bazelisk", "run", "//install", "--", "--bin", bin
+    system "bazelisk", "run", "//cli/homebrew:collect", "--", "build_artifacts"
+    bin.install "build_artifacts/aspect" => "aspect"
   end
 
   test do
