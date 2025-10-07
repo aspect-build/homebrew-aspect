e1b9fa8da51e5e91b0fd433b480773ee47639e8f
Greg Magolan <greg@aspect.build>
2025-10-06 19:24:30 -0700

    Update brew formulas for Aspect CLI 2025.41.8

--- Formula/aspect.rb
+++ Formula/aspect.rb
@@ -2,15 +2,15 @@ class Aspect < Formula
   desc "Correct, fast, usable: choose three"
   homepage "https://aspect.build"
   url "https://github.com/aspect-build"
-  version "2025.41.5"
+  version "2025.41.8"
   license "Apache-2.0"
   bottle do
-    root_url "https://github.com/aspect-build/aspect-cli/releases/download/2025.41.5"
-    sha256 cellar: :any_skip_relocation, monterey: "a8c7541aaea363724ab81dca6d4c6093e7985c57d2d8eafe019c5878102fa0a5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bba9d872c51761510c3f459c0a2a3464ebfc7da3a86ad99f5d4b0c6d7d030130"
-    sha256 cellar: :any_skip_relocation, big_sur: "937cb2695e1a6a745778536dc35e6e1ecd652fb17c8d3691565e3d675d48e9b8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b4bcfee5d946e4baf91cb271769dcd896746308599f9123bb1e3c55be5c40460"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e0b6d33449ca8727800de7b0edb949be7c9db726db9288c25450e20e704ad567"
+    root_url "https://github.com/aspect-build/aspect-cli/releases/download/2025.41.8"
+    sha256 cellar: :any_skip_relocation, monterey: "2ed3ee890c4677041b270dd378edc1ba02c968cbe7bfbad4229c3f5388e1a85a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "63fa9dd866804052ecf8714d7b178dc723bd963d423a947f4a861a657ae9fefc"
+    sha256 cellar: :any_skip_relocation, big_sur: "b14aaeb6032e1f13aed645c21d91155657613c1c2b96b6e1dbb9eea53056a170"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c767e4f81259641135382ff745965ff855e12271f19e4e2bf7c82597f8bad222"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8245c26f788ae6870a608801224114f358da802c17cb4590171ffde681aae9a2"
   end
 
   conflicts_with "bazel", because: "aspect replaces the bazel binary"
