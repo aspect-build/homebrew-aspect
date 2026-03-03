53f932c2488da69cc0c02c9a9698acad1d40a10f
Greg Magolan <greg@aspect.build>
2026-03-02 18:18:05 -0800

    Update brew formulas for Aspect CLI v2026.10.1

--- Formula/aspect.rb
+++ Formula/aspect.rb
@@ -2,15 +2,15 @@ class Aspect < Formula
   desc "Correct, fast, usable: choose three"
   homepage "https://aspect.build"
   url "https://github.com/aspect-build/aspect-cli"
-  version "2026.1.7"
+  version "2026.10.1"
   license "Apache-2.0"
   bottle do
-    root_url "https://github.com/aspect-build/aspect-cli/releases/download/v2026.1.7"
-    sha256 cellar: :any_skip_relocation, monterey: "c6adead8095578863aa2cfe3eae6df3a73eb14f2e28185e8043dfee7fbceab77"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "614d9a3869fbccaf4af8a38e4bccfc49c06a37df5e0af38dc355fd71018f7d16"
-    sha256 cellar: :any_skip_relocation, big_sur: "2a4157169e6e4a5a677dba1edd8213f28edef2fbec1cc47775962f3552253cbe"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b721282b67cdfdf3b76722f68e933613b3d4086c8ef68c0bea259e11337ea561"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "99aa6d979c202f8558774a8df9547e990b6d0d9542aec8c635f2585e23c39b34"
-    sha256 cellar: :any_skip_relocation, arm64_linux: "6f449212a8716957ec864ad6af3b52a4709ea5b4f5ff8ffd648aae0c10beac0b"
+    root_url "https://github.com/aspect-build/aspect-cli/releases/download/v2026.10.1"
+    sha256 cellar: :any_skip_relocation, monterey: "cca561c1430917eeb8a3234c6c90fc7fb4459bff8873c19eb5c972d6ea3468b1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b6e647f315364158a12ab02ef8c475a1fffa6d745382b1c56107d82488a6b7db"
+    sha256 cellar: :any_skip_relocation, big_sur: "0fdb3c6597ebece18996db016453acd922b4182447f4c7651a9a2681aa04fd1a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4c664b6b985201bc2e4b5f4e5fb5990a9189492ddb05dedb3725cd22543ca741"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "dba8fd21a95c2f776455d7ff9e5a629d4067a2fc883164d8281ed1486e2f23fa"
+    sha256 cellar: :any_skip_relocation, arm64_linux: "68921bf6a3b8154d1cb3d2a915aca01ff9aa7e2b9eb7843005613675fbc0c4b0"
   end
 end
